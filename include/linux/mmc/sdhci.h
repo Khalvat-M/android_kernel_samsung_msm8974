a738f71dafdc39a62eb3d56ba2bccce2465162ae
Aaron Lu <aaron.lu@amd.com>
2012-07-04 13:29:09 +0800

    mmc: sdhci: Introduce new flag SDHCI_USING_RETUNING_TIMER
    Add a new flag of SDHCI_USING_RETUNING_TIMER to represent if the host
    is using a retuning timer for the card inserted.
    
    This flag is set when the host does tuning the first time for the card
    and the host's retuning mode is 1. This flag is used afterwards whenever
    needs to decide if the host is currently using a retuning timer.
    
    This flag is cleared when the card is removed in sdhci_reinit.
    
    The set/clear of the flag and the start/stop of the retuning timer is
    associated with the card's init/remove time, so there is no need to
    touch it when the host is to be removed as at that time the card should
    have already been removed.
    
    Signed-off-by: Aaron Lu <aaron.lu@amd.com>
    Reviewed-by: Girish K S <girish.shivananjappa@linaro.org>
    Reviewed-by: Philip Rakity <prakity@marvell.com>
    Signed-off-by: Chris Ball <cjb@laptop.org>

--- include/linux/mmc/sdhci.h
+++ include/linux/mmc/sdhci.h
@@ -220,6 +220,7 @@ struct sdhci_host {
 #define SDHCI_SDIO_IRQ_ENABLED	(1<<9)	/* SDIO irq enabled */
 #define SDHCI_HS200_NEEDS_TUNING (1<<10)	/* HS200 needs tuning */
 #define SDHCI_HS400_NEEDS_TUNING (1<<11)	/* HS400 needs tuning */
+#define SDHCI_USING_RETUNING_TIMER (1<<12)	/* Host is using a retuning timer for the card */
 
 	unsigned int version;	/* SDHCI spec. version */
 
